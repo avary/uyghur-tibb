1f9e2b5af086775c9b344a96f9e8435707d7ba1b
rehimekohlevi265-maker <rehimekohlevi265@gmail.com>
2026-09-12 16:17:15 +0300

    ئارقا سەھنە كىرىش بىخەتەرلىكىنى كۈچەيتىش (admin/123456 تاقالدى) ۋە داۋاملاشتۇرۇش كۈنىنى باشقا قايتۇرۇش (v65)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'uytibb-v64-20260912_open_registration_no_gatekeeper_clean_slate';
+const CACHE_NAME = 'uytibb-v65-20260912_secure_admin_and_reset_streak';
 const ASSETS = [
   './',
   './index.html',
