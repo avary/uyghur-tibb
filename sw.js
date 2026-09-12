e9ade031f962fc83afaac7071ed0a3e401817c3d
rehimekohlevi265-maker <rehimekohlevi265@gmail.com>
2026-09-12 11:11:38 +0300

    تۈزىتىش v62: «خام دورىلار ئىلمى (1-قىسىم ۋە 2-قىسىم)» كىتابلىرى تولۇق قوشۇلدى، كۇتۇپخانىغا بىۋاسىتە ۋە ئىچكى كۆرۈش ئورۇنلاشتۇرۇلدى

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'uytibb-v61-20260912_indexeddb_pdf_store_and_embedded_viewer';
+const CACHE_NAME = 'uytibb-v62-20260912_kham_dorilar_1_2_integrated_and_viewer_direct_open';
 const ASSETS = [
   './',
   './index.html',
